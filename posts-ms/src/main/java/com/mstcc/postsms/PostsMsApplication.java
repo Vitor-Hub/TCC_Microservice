0103d7ff8c1967c1e90e1f79b5fff0e573c3e0c4
Vitor-Hub <rj.vitorsantos@gmail.com>
2024-03-01 23:57:31 -0300

    adding user

--- posts-ms/src/main/java/com/mstcc/postsms/PostsMsApplication.java
+++ posts-ms/src/main/java/com/mstcc/postsms/PostsMsApplication.java
@@ -1,6 +1,6 @@
-package com.mstcc;
+package com.mstcc.postsms;
 
-public class Main {
+public class PostsMsApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ users-ms/src/main/java/com/mstcc/userms/controllers/UserController.java
@@ -0,0 +1,46 @@
+package com.mstcc.userms.controllers;
+
+import com.mstcc.userms.entities.User;
+import com.mstcc.userms.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.findUserById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+}
