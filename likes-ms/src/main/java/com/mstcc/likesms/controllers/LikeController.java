6ab7c9991255cf6316ae5dd0c2ea5c599fd03ecf
Vitor-Hub <rj.vitorsantos@gmail.com>
2024-03-03 13:48:09 -0300

    Fixing Like Entity

--- likes-ms/src/main/java/com/mstcc/likesms/controllers/LikeController.java
+++ likes-ms/src/main/java/com/mstcc/likesms/controllers/LikeController.java
@@ -1,2 +1,38 @@
-package com.mstcc.likesms.controllers;public class LikeController {
+package com.mstcc.likesms.controllers;
+
+import com.mstcc.likesms.entities.Like;
+import com.mstcc.likesms.services.LikeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/likes")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @Autowired
+    public LikeController(LikeService likeService) {
+        this.likeService = likeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Like> createLike(@RequestBody Like like) {
+        Like savedLike = likeService.saveLike(like);
+        return ResponseEntity.ok(savedLike);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Like> getLikeById(@PathVariable Long id) {
+        return likeService.getLikeById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLike(@PathVariable Long id) {
+        likeService.deleteLike(id);
+        return ResponseEntity.ok().build();
+    }
 }
--- likes-ms/src/main/java/com/mstcc/likesms/services/LikeService.java
+++ likes-ms/src/main/java/com/mstcc/likesms/services/LikeService.java
@@ -1,2 +1,31 @@
-package com.mstcc.likesms.services;public class LikeService {
+package com.mstcc.likesms.services;
+
+import com.mstcc.likesms.entities.Like;
+import com.mstcc.likesms.repositories.LikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+
+    @Autowired
+    public LikeService(LikeRepository likeRepository) {
+        this.likeRepository = likeRepository;
+    }
+
+    public Like saveLike(Like like) {
+        return likeRepository.save(like);
+    }
+
+    public Optional<Like> getLikeById(Long id) {
+        return likeRepository.findById(id);
+    }
+
+    public void deleteLike(Long id) {
+        likeRepository.deleteById(id);
+    }
 }
